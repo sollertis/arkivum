1297fb04d4806493d96feeb562b1636f64dfa359
Richard Lowe <richard.lowe@arkivum.com>
2013-03-22 14:53:58 +0000

    Initial commit of Arkivum storage plugin for EPrints.

---
+++ dist/lib/epm/arkivum/cfg/cfg.d/arkivum.pl
@@ -0,0 +1,6 @@
+# To Configure your Arkivum Storage, please fill in the configuration fields below:
+#
+# Once done, please remove the comments '#' from the beginning of each line, save this file and then reload the repository config from the Admin screen.
+# 
+
+#$c->{plugins}->{"Storage::Arkivum"}->{params}->{mount_path} = "/mnt/archive";
